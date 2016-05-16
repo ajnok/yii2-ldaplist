10cf49aa343ff6d47bb0a120c31d75b15acff381
ajnok <ajnokit@gmail.com>
2016-05-17 00:53:55 +0700

    Add translation

--- Module.php
+++ Module.php
@@ -8,11 +8,23 @@
 
 namespace ajnok\ldaplist;
 
+use Yii;
 class Module extends \yii\base\Module
 {
     public function init()
     {
         parent::init();
-        
+        $this->_registerTranslations();
     }
+    private function _registerTranslations()
+    {
+        if (!isset(Yii::$app->i18n->translations['ldaplist'])) {
+            Yii::$app->i18n->translations['ldaplist'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en-US',
+                'basePath' => '@ajnok/ldaplist/messages',
+            ];
+        }
+    }
+
 }
---
+++ messages/th/ldaplist.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ajnok
+ * Date: 5/17/2016 AD
+ * Time: 00:41
+ */
+return [
+    'system' => 'ระบบ',
+];
